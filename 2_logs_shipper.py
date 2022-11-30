7886cb15fda7dc43ef9b9f268f47310646aebf55
rickdat <80357022+rickdat@users.noreply.github.com>
2022-11-29 21:17:18 -0500

    Update 2_logs_shipper.py

--- 2_logs_shipper.py
+++ 2_logs_shipper.py
@@ -11,6 +11,7 @@ rand_time = lambda: float(random.randrange(0,30))+time_uuid.utctime()
 now = datetime.now()
 service_url = 'pulsar+ssl://pulsar-gcp-uscentral1.streaming.datastax.com:6651'
 token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NjkyNTYwMTksImlzcyI6ImRhdGFzdGF4Iiwic3ViIjoiY2xpZW50O2I0Y2M4NzgwLTg1NzQtNGQwMS1hMzkzLWE0MzU4MDcyYjhhODtaR0YwWVhOMFlYZ3RZWFJ3OzI2ZmM2YTJiMmYiLCJ0b2tlbmlkIjoiMjZmYzZhMmIyZiJ9.V_7uF1h_2W3rpCIcAhu0HhlkqDfRgqi2A76yBc5mdwHVsDqadz4BLpvcD7hZdpUP1vbbb318PrbCEdpGbxyEmNeE_opEaKh2yNUh27QELUVxU-LqcREieLjDJEmwDNQz6Sm3MDd1fmUkefjJgdxkqhE4Vf0R2npqBaAxdfgfrjAFOVPUePJswCf74M20vTMFkpqj0A7ryyslKPG_2izwpTYZwaQ-qS5mas2xDICsOdvRm4s1yyMP5P3ihmzuJa9net0p7JLnlglz0FkoqBUjUQ5z4uLcFxYW1oi2OoMLfHWBujjFtdlDzq-IojsSkCigQFpkNQFaR-YsRYS8VTjwwA"
+topic = 'persistent://datastax-atp/events-namespace/events-topic'
 
 class Events(Record):
     device_name = String() 
@@ -21,7 +22,7 @@ class Events(Record):
 client = pulsar.Client(service_url,
                         authentication=pulsar.AuthenticationToken(token))
 
-producer = client.create_producer('persistent://datastax-atp/events-namespace/events-topic',schema=JsonSchema(Events))
+producer = client.create_producer(topic,schema=JsonSchema(Events))
 
 # Function to read ubuntu system logs. Remove the "." before /var when in a real environment.
 def get_logs():
